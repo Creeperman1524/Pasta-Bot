3962fb62632cdaae9feacb45758947f946bc84e2
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2022-06-29 20:12:45 -0400

    Database connection
    Connected to the database and alerted when disconnected

---
+++ src/tasks/databaseConnection.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { logger } = require('../logging.js');
+
+module.exports = {
+	name: 'databaseConnection',
+	mode: 'ONCE',
+
+	async execute() {
+		// Connects to the database
+		mongoose.Promise = global.Promise;
+		await mongoose.connect(process.env.mongoDB, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
+	},
+};
+
+// Alerts about connection, outages, and errors
+mongoose.connection.once('connected', () => {
+	logger.child({ mode: 'DATABASE' }).info('Connected to database');
+});
+
+mongoose.connection.once('disconnected', () => {
+	logger.child({ mode: 'DATABASE' }).warn('Disconnected from the database');
+});
+
+mongoose.connection.once('reconnected', () => {
+	logger.child({ mode: 'DATABASE' }).info('Reconnected to the database');
+});
+
+mongoose.connection.on('error', (error) => {
+	logger.child({ mode: 'DATABASE' }).error(error);
+});
