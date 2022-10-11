ba7ce71bdcb6ccddede649e2cd11de5593cf6aca
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2022-10-11 16:21:26 -0400

    Formatting issues
    Fixed formatting issues from the last commit

--- src/commands/minigames/minesweeper.js
+++ src/commands/minigames/minesweeper.js
@@ -538,7 +538,7 @@ async function generateStatsEmbed(interaction) {
 				**Wins**: \`${stats.wins}\`
 				**Total Games**: \`${stats.totalGames}\`
 				**Win Ratio**: \`${Math.round((stats.wins / stats.totalGames) * 1000) / 10}%\`
-				**Fastest Time**: \`${stats.wins > 0 ? stats.fastestTime : 'This player has not won a game!'}s\``,
+				**Fastest Time**: \`${stats.wins > 0 ? stats.fastestTime : 'This player has not won a game!'}\``,
 			);
 
 		interaction.editReply({ embeds: [statsEmbed] });
@@ -563,7 +563,7 @@ async function generateStatsEmbed(interaction) {
 				**Wins**: \`${stats.wins}\`
 				**Total Games**: \`${stats.totalGames}\`
 				**Win Ratio**: \`${Math.round((stats.wins / stats.totalGames) * 1000) / 10}%\`
-				**Fastest Time**: \`${stats.wins > 0 ? stats.fastestTime : 'This player has not won a game!'}s\``,
+				**Fastest Time**: \`${stats.wins > 0 ? stats.fastestTime : 'This player has not won a game!'}\``,
 			);
 
 		interaction.editReply({ embeds: [statsEmbed] });
