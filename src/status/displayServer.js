e787bc0895500a979ee09695d86a664766887085
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2022-10-23 21:32:50 -0400

    Added activity system
    Created the activity system to cycle through different activities that the bot can show to other people
    
    Updated displayServer to now use this system and take priority over the icon

---
+++ src/status/displayServer.js
@@ -0,0 +1,51 @@
+const mcping = require('mcping-js');
+
+const { mcServerPort } = require('../config.json');
+
+module.exports = {
+	name: 'displayServer',
+
+	// Returns the status info on the minecraft server
+	async execute() {
+		const server = new mcping.MinecraftServer(process.env.mcServerIP, mcServerPort);
+
+		let activity = '';
+		let status = 'dnd';
+
+		return new Promise((resolve) => {
+
+			// Pings the server for information
+			server.ping(1000, 758, (err, res) => {
+				// Server offline/errored
+				if (!(typeof err === 'undefined' || err === null)) {
+					// console.log(err);
+					resolve(['an offline server :(', 'dnd']);
+				}
+
+				// Server online with no players
+				if (typeof res.players.sample === 'undefined') {
+					status = 'idle';
+					activity = res.players.online + '/' + res.players.max + ' players';
+				}
+
+				// Gets the online players
+				let onlinePlayers = [];
+
+				// Server online with players
+				if (!(typeof res.players.sample === 'undefined')) {
+					status = 'online';
+
+					for (let i = 0; i < res.players.sample.length; i++) {
+						onlinePlayers.push(res.players.sample[i].name);
+					}
+					onlinePlayers = onlinePlayers.sort().join(', ');
+
+					activity = res.players.online + '/' + res.players.max + ' players -\n ' + onlinePlayers;
+				}
+
+				// Returns the information on the server
+				resolve([activity, status]);
+			});
+		});
+	},
+};
---
+++ src/tasks/statusSystem.js
@@ -0,0 +1,61 @@
+const { logger } = require('../logging');
+const fs = require('fs');
+const { statusInterval } = require('../config.json');
+
+const functions = [];
+const statusFiles = fs.readdirSync('./src/status');
+
+// Gather activity files from folder
+for (const file of statusFiles) {
+	const func = require(`../status/${file}`);
+	functions.push(func);
+}
+
+// Loop index
+let i = 0;
+
+function iterateCounter(presences) {
+	i++; // Initially increment the counter
+	// Skip over the server status if there are no players online (as long as there are more statuses to be shown)
+	if(Object.keys(presences)[i] == 'displayServer' && presences['displayServer'][1] !== 'online' && functions.length > 1) {
+		i++;
+	}
+	// Overflow check
+	if(i > functions.length - 1) i = 0;
+}
+
+module.exports = {
+	name: 'statusSystem',
+	mode: 'INTERVAL',
+	interval: statusInterval,
+
+	async execute(client) {
+		// The presence that will be displayed
+		const botPresence = ['', 'dnd'];
+
+		// Stores what all of the functions want to be displayed
+		const presences = {};
+		for(const func of functions) {
+			presences[func.name] = await func.execute(client);
+		}
+
+		// Always display's the status as the server's status
+		botPresence[1] = presences['displayServer'][1];
+
+		// Displays the function on this cycle
+		botPresence[0] = presences[Object.keys(presences)[i]][0];
+
+		// Iterates the counter
+		iterateCounter(presences);
+
+		// Set's the bot's presence
+		client.user.setPresence({
+			activities: [{
+				name: botPresence[0],
+				type: 'WATCHING',
+			}],
+			status: botPresence[1],
+		});
+		logger.child({ mode: 'STATUS' }).debug(`Status has been updated with status '${botPresence[1]}' and activity '${botPresence[0]}'`);
+	},
+};
