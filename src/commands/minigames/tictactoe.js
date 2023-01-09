0c13bca1901dcca871cdb62d223ca74ddd63c62f
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2023-01-08 21:04:59 -0500

    Player duel requests
    Added the "duel" request system so a user can play against another user

--- src/commands/minigames/tictactoe.js
+++ src/commands/minigames/tictactoe.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Collection, MessageActionRow, MessageButton } = require('discord.js');
+const { Collection, MessageActionRow, MessageButton } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { newEmbed, colors } = require('../../util/embeds.js');
 const { logger } = require('../../logging.js');
@@ -9,17 +9,23 @@ const emojis = {
 	'blank' : '➖',
 	'X': '❌',
 	'O': '⭕',
+	'confirm': '✔️',
+	'deny': '✖️',
 };
 
+// Creates a new game for the user
 function createGame(myInteraction) {
 	const game = {
-		board: [],
-		buttons: [],
-		player1Turn: false,
-		componentCollector: null,
-		interaction: myInteraction, // The user command
-		embed: null, // The game/embed sent
-		timeout: 10 * 60000, // The expiration timer for the gamee
+		board: [], 					// The internal array of the board
+		buttons: [],				// The button representation of the board
+		confirmation: [], 			// The buttons to accept or deny the play request
+		player1Turn: false, 		// Whether or not it's player1's turn
+		player2: null, 				// The user object of player2
+		player2Accepted: false, 	// Whether or not player2 accepeted the game
+		componentCollector: null,	// The main component collect for the game
+		interaction: myInteraction,	// The user command
+		embed: null, 				// The game/embed sent
+		timeout: 10 * 60000, 		// The expiration timer for the gamee
 	};
 
 	// Adds it to the game object
@@ -27,6 +33,7 @@ function createGame(myInteraction) {
 	startGame(game);
 }
 
+// Starts a new game
 function startGame(game) {
 
 	// Creates the board of buttons
@@ -50,30 +57,124 @@ function startGame(game) {
 		);
 	game.buttons = [row1, row2, row3];
 
-	const tictactoeEmbed = newEmbed()
-		.setTitle('TicTacToe')
-		.setColor(colors.yes)
-		.setDescription('A game of tictactoe!');
-
-	// Sends the game to the user and waits for input
-	game.interaction.editReply({ embeds: [tictactoeEmbed], components:[row1, row2, row3] }).then(async embed => {
-		game.embed = embed;
-
-		// Wait for input
-	});
-
+	if(!game.interaction.options.getUser('user')) {
+		// Playing against the bot
+		startGameBot(game);
+	} else {
+		// Playing against a user
+		startGameUser(game);
+	}
 }
 
+// A helper function to add buttons
 function createButton(ID, emoji, style) {
 	return new MessageButton().setCustomId(ID).setEmoji(emoji).setStyle(style);
 }
 
+// Initializes the game to play agains the bot
+function startGameBot(game) {
+	game.player2 = 'bot';
+
+	console.log('playing against bot');
+
+	// TODO: initialize the game for the bot
+}
+
+// Initializes the game to play against another user
+function startGameUser(game) {
+	game.player2 = game.interaction.options.getUser('user');
+
+	const confirmationRow = new MessageActionRow()
+		.addComponents(
+			createButton('yes', emojis.confirm, 'SUCCESS'),
+			createButton('no', emojis.deny, 'DANGER'),
+		);
+
+	// Embed to ask player2
+	const requestEmbed = newEmbed()
+		.setTitle('Tic-Tac-Toe')
+		.setColor(colors.tictactoeCommand)
+		.setDescription(`<@${game.interaction.user.id}> is challenging you to a game!\nReact below if you wish to accept!`)
+		.addFields({
+			name: 'Time to Accept',
+			value: `<t:${Math.round((new Date().getTime() + 60000) / 1000)}:R>`,
+			inline: true,
+		});
+
+	// Sends the game to the channel and waits for confirmation
+	game.interaction.editReply({ content: `<@${game.player2.id}>`, embeds: [requestEmbed], components:[confirmationRow] }).then(async embed => {
+		game.embed = embed;
+
+		// Waits to accept request
+		const confirmationCollector = embed.createMessageComponentCollector({ componentType: 'BUTTON', time: 1 * 60000 });
+		confirmationCollector.on('collect', (button) => {
+			button.deferUpdate();
+			if(button.user.id !== game.player2.id) return;
+
+			if(button.customId == 'no' && !game.player2Accepted) deniedRequest(game, false);
+
+			game.player2Accepted = true;
+			confirmationCollector.stop(); // Disposes of the collector
+
+			// TODO: initialize the game board, awaitInput()
+		});
+
+		// Ran out of time
+		confirmationCollector.on('end', () => {
+			if(!game.player2Accepted) deniedRequest(game, true);
+		});
+	});
+}
+
+// If the user denies the request, delete the game from memory
+function deniedRequest(game, timeout) {
+	const notAcceptEmbed = newEmbed()
+		.setTitle(timeout ? 'Request not accepted!' : 'Request denied!')
+		.setColor(colors.tictactoeCommand)
+		.setDescription(`<@${game.player2.id}> did not accept the request :(`);
+
+	game.interaction.editReply({ embeds: [notAcceptEmbed], components: [] });
+
+	games.delete(game.interaction.id);
+}
+
+function awaitInput(game) {
+	game.componentCollector = game.embed.createMessageComponentCollector({ componentType: 'BUTTON', time: game.timeout });
+
+	game.componentCollector.on('collect', (button) => {
+		button.deferUpdate();
+
+		// TODO: playing the game
+	});
+
+	game.componentCollector.on('end', () => {
+		// TODO: ran out of time
+	});
+
+}
+
+// The discord command bits
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('tictactoe')
-		.setDescription('A tic-tac-toe minigame'),
+		.setDescription('A tic-tac-toe minigame')
+
+		// start game
+		.addSubcommand(subcommand => subcommand
+			.setName('start')
+			.setDescription('Start a game of tictactoe')
+			.addUserOption(option => option
+				.setName('user')
+				.setDescription('The user you wish to play against'),
+			),
+		),
 
 	execute(interaction) {
-		createGame(interaction);
+		switch (interaction.options.getSubcommand()) {
+		case 'start':
+			createGame(interaction);
+			break;
+		}
+
 	},
 };
