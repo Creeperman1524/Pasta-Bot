316f1b9e01f7fc9aaf48ec0ad375e186a04f8e42
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2023-12-10 18:49:06 -0500

    Fixed incorrect december birthdays
    Fixed a bug where december birthdays were skipped with the logic for checking next year's birthdays

--- src/status/birthdayDisplay.js
+++ src/status/birthdayDisplay.js
@@ -7,22 +7,29 @@ module.exports = {
 		const now = new Date();
 
 		let birthday;
-		for(const b in birthdays) {
-			birthday = b;
-			// Finds the first birthday that's after today
-			// or currently today to display before 7pm
-			const bdate = convertDate(birthdays[b], now);
-			if(bdate.getTime() > now || (bdate.getMonth() == now.getMonth() && bdate.getDate() == now.getDate() && now.getHours() < 19)) {
-				break;
+		let checkNextYear = false;
+		let found = false;
+
+		loop:
+		while (!found) {
+			for(const b in birthdays) {
+				birthday = b;
+				// Finds the first birthday that's after today
+				// or currently today to display before 7pm
+				const bdate = convertDate(birthdays[b], now, checkNextYear);
+				if(bdate.getTime() > now || (bdate.getMonth() == now.getMonth() && bdate.getDate() == now.getDate() && now.getHours() < 19)) {
+					found = true;
+					break loop;
+				}
 			}
+			checkNextYear = true;
 		}
 
 		// Calculates the seconds, days, hours, and minutes until the birthday
-		const seconds = (convertDate(birthdays[birthday], now).getTime() - now) / 1000;
+		const seconds = (convertDate(birthdays[birthday], now, checkNextYear).getTime() - now) / 1000;
 		const days = Math.floor(seconds / 24 / 60 / 60);
 		const hours = Math.floor(seconds % (60 * 60 * 24) / (60 * 60));
 		const minutes = Math.floor(seconds % (60 * 60) / 60);
-
 
 		if(days >= 1) { // Further than 1 day away
 			return [`${days}d ${hours}h 🎂 ${birthday}`, ''];
@@ -34,8 +41,6 @@ module.exports = {
 	},
 };
 
-function convertDate(arr, day) {
-	// If the current month is December, we want to check January birthdays for the next year
-	const year = arr[0] == 1 && day.getMonth() == 11 ? day.getFullYear() + 1 : day.getFullYear();
-	return new Date(year, arr[0] - 1, arr[1]);
+function convertDate(arr, day, checkNextYear) {
+	return new Date(checkNextYear ? day.getFullYear() + 1 : day.getFullYear(), arr[0] - 1, arr[1]);
 }
