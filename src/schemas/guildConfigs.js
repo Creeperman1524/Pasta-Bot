e0bbd867528e6f18a78ad7793e766160d2cd4149
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2024-07-22 23:52:07 -0400

    Updated guild schema
    Updated the guild database schema to contain more options for the configs
    Fixed the reaction roles to adapt to this new format

--- src/schemas/guildConfigs.js
+++ src/schemas/guildConfigs.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const guildConfigSchema = new mongoose.Schema({
 	guildID: { type: String, required: true },
-	reactionMessages: { type: {}, required: false },
-	loggingChannelID: { type: String, required: false },
 
-	lastEdited: { type: String, required: true },
+	modules: { type: {}, required: false }, // The modules enabled for the guild
+
+	reactionMessages: { type: {}, required: false }, // Contains the reaction message
+	valorantRoles: { type: {}, required: false }, // Contains the valorant role data
+
+	loggingChannelID: { type: String, required: false }, // The logging channel
 }, { minimize: false });
 
 module.exports = mongoose.model('GuildConfig', guildConfigSchema);
